355373a2f1262f3ccafebe69f24638b4780bb3ac
zdanovich89 <zdanovich89@gmail.com>
2019-03-20 01:10:30 +0300

    fix: fix bug on code

--- src/index.js
+++ src/index.js
@@ -5,17 +5,19 @@
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
   let length = preferences.length; 
-  let sum = 0;  
+  let count = 0;  
   for (let i = 0; i<length; i++) {
-    let firstSpich = preferences [i] - 1;
-    if (firstSpich = i) continue;
-    let secondSpich = preferences [firstSpich] - 1;
-    if (secondSpich = firstSpich) continue;
-    let thirdSpich = preferences [secondSpich] - 1;
-    if ( thirdSpich = i) {
-      sum++;
+    let firstSpich = preferences[i] - 1;
+    if (firstSpich === i) continue;
+    let secondSpich = preferences[firstSpich] - 1;
+    if (secondSpich === firstSpich) continue;
+    let thirdSpich = preferences[secondSpich] - 1;
+    if (thirdSpich === secondSpich) continue;
+    if (thirdSpich === i) {
+      count++;
     }
-    return sum;
+  }
+    return count;
 
-  } 
+   
 }
